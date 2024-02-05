9e06780622cb127b267385af625c5237404dcab3
magecode <magecode7@yandex.ru>
2024-02-05 15:36:49 +0300

    add timer

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ API_KEY = os.environ.get(
     "CTFD_API_KEY",
     "ctfd_2c71a65dc478092a47cdebf982b250337ba33b3f15505001fd0dbec10f62b89e",
 )
-API_URL = os.environ.get("CTFD_API_URL", "https://krductf.ru/api/v1/scoreboard")
+API_URL = os.environ.get("CTFD_URL", "https://krductf.ru")
+API_SCOREBOARD_URL = f"""{API_URL}/api/v1/scoreboard"""
+API_START_TIME_URL = f"""{API_URL}/api/v1/configs/start"""
+API_END_TIME_URL = f"""{API_URL}/api/v1/configs/end"""
 
 headers = {
     "Authorization": f"Token {API_KEY}",
@@ -17,12 +20,29 @@ headers = {
 
 @app.route("/api/scoreboard")
 def get_scoreboard():
-    response = requests.get(API_URL, headers=headers)
+    response = requests.get(API_SCOREBOARD_URL, headers=headers)
     if response.status_code == 200:
-        # scoreboard = json.loads(open("users.json").read())
-        scoreboard = response.json()["data"]
-        return jsonify(scoreboard)
-    return jsonify({"error": "Failed to fetch data"}), 500
+        data = response.json()["data"]
+        return jsonify(data)
+    return jsonify({"error": "Failed to fetch scoreboard"}), 500
+
+
+@app.route("/api/start")
+def get_start_time():
+    response = requests.get(API_START_TIME_URL, headers=headers)
+    if response.status_code == 200:
+        value = response.json()["data"]["value"]
+        return jsonify(value)
+    return jsonify({"error": "Failed to fetch start time"}), 500
+
+
+@app.route("/api/end")
+def get_end_time():
+    response = requests.get(API_END_TIME_URL, headers=headers)
+    if response.status_code == 200:
+        value = response.json()["data"]["value"]
+        return jsonify(value)
+    return jsonify({"error": "Failed to fetch end time"}), 500
 
 
 @app.route("/")
---
+++ static/js/time_update.js
@@ -0,0 +1,68 @@
+let startTime;
+let endTime;
+
+function fetchStartTime() {
+  fetch("/api/start")
+    .then((response) => response.json())
+    .then((data) => {
+      startTime = new Date(data * 1000).getTime();
+      updateStartTime();
+    })
+    .catch((error) => {
+      console.error("Error fetching start time:", error);
+    });
+}
+
+function fetchEndTime() {
+  fetch("/api/end")
+    .then((response) => response.json())
+    .then((data) => {
+      endTime = new Date(data * 1000).getTime();
+      updateEndTime();
+    })
+    .catch((error) => {
+      console.error("Error fetching end time:", error);
+    });
+}
+
+fetchStartTime();
+fetchEndTime();
+
+setInterval(function () {
+  let now = new Date().getTime();
+  let distance = startTime - now;
+
+  if (distance > 0) {
+    document.getElementById("time").innerHTML =
+      "До начала:<div class='time-countdown'>" + timeToStr(distance) + "</div>";
+  } else {
+    distance = endTime - now;
+
+    if (distance > 0) {
+      document.getElementById("time").innerHTML =
+        "До окончания:<div class='time-countdown'>" +
+        timeToStr(distance) +
+        "</div>";
+    }
+  }
+}, 1);
+
+function timeToStr(time) {
+  let days = Math.floor(time / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((time % (1000 * 60)) / 1000);
+  let s = "";
+  if (days > 0) s += days + ":";
+  if (days > 0 || hours > 0) s += hours + ":";
+  if (days > 0 || hours > 0 || minutes > 0) s += minutes + ":";
+  s += seconds;
+  if (minutes < 5)
+    s =
+      "<span class='alert-red'>" +
+      s +
+      ":" +
+      Math.floor(time % 1000) +
+      "</span>";
+  return s;
+}
